341029777940017e6c9b5f8deda0d594132def0c
Wasilisk <vasylpetryna2002@gmail.com>
2026-03-09 17:36:56 +0000

    feat: add useAdaptiveField composable for conditional field validation in forms

---
+++ app/composables/useAdaptiveField.ts
@@ -0,0 +1,32 @@
+import type { Ref } from 'vue'
+import type { BaseFieldProps, GenericObject, FormContext, Path } from 'vee-validate'
+
+type FieldConfig = {
+    validateOnBlur?: boolean
+    validateOnChange?: boolean
+    validateOnInput?: boolean
+    validateOnModelUpdate?: boolean
+}
+
+type DefineFieldFn<TValues extends GenericObject> = FormContext<TValues>['defineField']
+
+export function useAdaptiveField<TValues extends GenericObject>(
+    defineField: DefineFieldFn<TValues>,
+    errors: Ref<Partial<Record<keyof TValues, string | undefined>>>
+) {
+    const baseConfig: FieldConfig = {
+        validateOnBlur: true,
+        validateOnChange: false,
+        validateOnInput: false,
+        validateOnModelUpdate: false,
+    }
+
+    return function <TKey extends keyof TValues & string>(
+        name: TKey
+    ): [Ref<TValues[TKey]>, Ref<BaseFieldProps & GenericObject>] {
+        return defineField(name as unknown as Path<TValues>, () => ({
+            ...baseConfig,
+            validateOnInput: !!errors.value[name],
+        })) as [Ref<TValues[TKey]>, Ref<BaseFieldProps & GenericObject>]
+    }
+}
