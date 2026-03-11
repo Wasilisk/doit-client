c80bfcef6b57a69ce7238872d17d9c0d21300d50
Wasilisk <vasylpetryna2002@gmail.com>
2026-03-11 18:20:15 +0000

    refactor(api): change response handling

--- app/composables/useApiClient.ts
+++ app/composables/useApiClient.ts
@@ -1,7 +1,3 @@
-interface ApiResponse<T> {
-    data: T
-}
-
 type RequestBody =
     | BodyInit
     | Record<string, any>
@@ -11,40 +7,15 @@ type RequestBody =
 export const useApiClient = () => {
     const { $api } = useNuxtApp()
 
-    const get = async <TRes>(url: string) =>
-        $api<ApiResponse<TRes>>(url).then(r => r.data)
+    const get = async <TRes>(url: string) => $api<TRes>(url)
 
-    const post = async <TReq extends RequestBody, TRes>(
-        url: string,
-        body: TReq
-    ) =>
-        $api<ApiResponse<TRes>>(url, {
-            method: 'POST',
-            body,
-        }).then(r => r.data)
+    const post = async <TReq extends RequestBody, TRes>(url: string, body: TReq) => $api<TRes>(url, { method: 'POST', body, })
 
-    const put = async <TReq extends RequestBody, TRes>(
-        url: string,
-        body: TReq
-    ) =>
-        $api<ApiResponse<TRes>>(url, {
-            method: 'PUT',
-            body,
-        }).then(r => r.data)
+    const put = async <TReq extends RequestBody, TRes>(url: string, body: TReq) => $api<TRes>(url, { method: 'PUT', body, })
 
-    const patch = async <TReq extends RequestBody, TRes>(
-        url: string,
-        body: TReq
-    ) =>
-        $api<ApiResponse<TRes>>(url, {
-            method: 'PATCH',
-            body,
-        }).then(r => r.data)
+    const patch = async <TReq extends RequestBody, TRes>(url: string, body: TReq) => $api<TRes>(url, { method: 'PATCH', body, })
 
-    const del = async <TRes>(url: string) =>
-        $api<ApiResponse<TRes>>(url, {
-            method: 'DELETE',
-        }).then(r => r.data)
+    const del = async <TRes>(url: string) => $api<TRes>(url, { method: 'DELETE', })
 
     return { get, post, put, patch, del }
 }
