53a80a8d1076444c83396a344260d8abd1419581
Wasilisk <vasylpetryna2002@gmail.com>
2026-03-11 18:27:56 +0000

    feat(app): add 404 redirect to tasks page

---
+++ app/error.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { ROUTES } from './constants/routes';
+
+const localePath = useLocalePath()
+
+const props = defineProps<{
+    error: { statusCode: number }
+}>()
+
+if (props.error.statusCode === 404) {
+    await navigateTo(localePath(ROUTES.TASKS), { redirectCode: 301 })
+}
+</script>
