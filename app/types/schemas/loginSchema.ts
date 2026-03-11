53e7be16a6baa7b48169477c3588931359ff3a8b
Wasilisk <vasylpetryna2002@gmail.com>
2026-03-11 18:21:21 +0000

    refactor(validation): use nonempty instead of min with value 1 in schemas

--- app/types/schemas/loginSchema.ts
+++ app/types/schemas/loginSchema.ts
@@ -2,7 +2,7 @@ import { z } from 'zod'
 import { toTypedSchema } from '@vee-validate/zod'
 
 export const loginZodSchema = z.object({
-    email: z.string().email('email.invalid'),
+    email: z.string().nonempty('email.required').email('email.invalid'),
     password: z.string().min(6, 'password.minLength'),
 })
 
--- app/types/schemas/registerSchema.ts
+++ app/types/schemas/registerSchema.ts
@@ -2,10 +2,10 @@ import { z } from 'zod'
 import { toTypedSchema } from '@vee-validate/zod'
 
 export const registerZodSchema = z.object({
-    email: z.string().min(1, 'email.required').email('email.invalid'),
-    fullName: z.string().min(1, 'fullName.required'),
+    email: z.string().nonempty('email.required').email('email.invalid'),
+    fullName: z.string().nonempty('fullName.required'),
     password: z.string().min(6, 'password.minLength'),
-    confirmPassword: z.string()
+    confirmPassword: z.string().nonempty('confirmPassword.required'),
 }).refine(
     (data) => data.password === data.confirmPassword,
     { message: 'confirmPassword.notMatch', path: ['confirmPassword'] }
